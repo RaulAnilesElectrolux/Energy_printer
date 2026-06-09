0c35ec436aa01021b0932b678f168ec90b50affb
AnileRau <raul.aniles@electrolux.com>
2026-06-09 10:28:34 -0600

    Formatos

---
+++ Controllers/FormatosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Energy_printer.Controllers
+{
+    public class FormatosController : Controller
+    {
+        // GET: Formatos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
