61742bc7c0f6e2a911514790515c46628fbb9ecd
AnileRau <raul.aniles@electrolux.com>
2026-06-09 11:36:10 -0600

    Cambiando formato y agregando imagenes

--- Controllers/FormatosController.cs
+++ Controllers/FormatosController.cs
@@ -9,7 +9,22 @@ namespace Energy_printer.Controllers
     public class FormatosController : Controller
     {
         // GET: Formatos
-        public ActionResult Index()
+        public ActionResult Amarillo()
+        {
+            return View();
+        }
+
+        public ActionResult Blanco()
+        {
+            return View();
+        }
+
+        public ActionResult Formato2()
+        {
+            return View();
+        }
+
+        public ActionResult Formato4()
         {
             return View();
         }
